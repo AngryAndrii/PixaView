10a648d8971cf95f88d91c96fd3cd98b668c2c3e
Andrii Chychkan <mywork1728@gmail>
2024-09-20 22:52:53 +0300

    commet pre refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import SearchComponent from './components/SearchComponent/Search';
 import ImageComponent from './components/ImageComponent/ImageComponent';
 import { Layout } from './components/Layout/Layout';
@@ -9,9 +9,10 @@ const App: FC = () => {
   const [images, setImages] = useState<resData[]>([]);
   const [emptySearch, setEmptySearch] = useState<boolean>(false);
   const [notFound, setNotFound] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
 
   const getData = async (query: string) => {
-    const data: resData[] | null = await fetchImage(query, 1);
+    const data: resData[] | null = await fetchImage(query, page);
     if (data !== null && data?.length == 0) {
       console.log('data null')
       setNotFound(true)
@@ -23,11 +24,22 @@ const App: FC = () => {
     setEmptySearch(prevstate => !prevstate);
   };
 
+  const setNewPage = () => {
+    setPage(prev => prev += 1)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [page]);
+
   return (
     <Layout>
       <SearchComponent getData={getData} emptyQuery={emptyQuery} />
       {notFound ? <>no found, please type valid search</> : <ImageComponent data={images} />}
       {emptySearch ? <>no data</> : <ImageComponent data={images} />}
+      <button className='load-more-button' onClick={() => {
+        setNewPage();
+      }}>Next page</button>
     </Layout>
   );
 };
